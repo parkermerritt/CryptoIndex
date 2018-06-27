82e325dcdb6042f57dfaa5d20c2fa8d23e573b7e
Parker Merritt <parkermerritt@Parkers-MacBook-Pro.local>
2018-06-27 08:00:27 -0400

    add Pie.vue

---
+++ js/Pie.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id = "pie">
+  <p>{{ greeting }} World!</p>
+</div>
+</template>
+
+<script>
+module.exports = {
+  data: function () {
+    return {
+      greeting: 'Hello'
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
